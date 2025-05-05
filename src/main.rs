4b61c43a36714d658357bdeca8d37e607401ed09
LukeMc <lukemccampbell@roll20.net>
2025-05-05 10:19:50 -0400

    Cell/Table Count works

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::{bail, Result};
 use std::fs::File;
 use std::io::prelude::*;
+use std::os::unix::prelude::FileExt;
 
 fn main() -> Result<()> {
     // Parse arguments
@@ -23,11 +24,39 @@ fn main() -> Result<()> {
             #[allow(unused_variables)]
             let page_size = u16::from_be_bytes([header[16], header[17]]);
 
+            #[allow(unused_variables)]
+            let page_count = u32::from_be_bytes([header[28], header[29], header[30], header[31]]);
+           
+            // Page 0 - we know from challenges we only care about page first page
+            // going to run from 0 to 3096 (4096 - 100)
+            let mut page0_post_header: [u8; 3096] = [0; 3096];
+            file.read_exact_at(&mut page0_post_header, 100)?;
+            
+            #[allow(unused_variables)]
+            let page_type = u8::from_be_bytes([page0_post_header[0]]);
+            
+            #[allow(unused_variables)]
+            let cell_count = u16::from_be_bytes([page0_post_header[3],page0_post_header[4]]); 
+            
+            
+            eprintln!("{}", cell_count);
+            
+            /*
+               A value of 2 (0x02) means the page is an interior index b-tree page.
+    A value of 5 (0x05) means the page is an interior table b-tree page.
+    A value of 10 (0x0a) means the page is a leaf index b-tree page.
+    A value of 13 (0x0d) means the page is a leaf table b-tree page.
+             */
+            
             // You can use print statements as follows for debugging, they'll be visible when running tests.
             eprintln!("Logs from your program will appear here!");
 
-            // Uncomment this block to pass the first stage
+            eprintln!("Number of pages: {}", page_count);
+          
+
+
             println!("database page size: {}", page_size);
+            println!("number of tables: {}", cell_count);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
