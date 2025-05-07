6a64d9d716813fe9c5eb6f5d90a2c904033de46c
LukeMc <lukemccampbell@roll20.net>
2025-05-07 15:34:53 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,8 @@ fn main() -> Result<()> {
     match command.as_str() {
         ".dbinfo" => {
             //println!("Number of pages: {}", pz.number_of_pages);
-            println!("database page size: {}", pz.database_page_size);
             println!("number of tables: {}", pz.number_of_tables);
+            println!("database page size: {}", pz.database_page_size);
         }
         ".tables" => {
             //eprintln!("Number of pages: {}", pz.number_of_pages);
